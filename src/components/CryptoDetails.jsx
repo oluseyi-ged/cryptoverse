68efd32457396e68b7507ccbcdbc511448248076
oluseyi-ged <oluseyi.ged@gmail.com>
2021-09-21 17:07:34 +0100

    line chart implemented

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -15,7 +15,11 @@ import {
   ThunderboltOutlined,
 } from "@ant-design/icons"
 
-import { useGetCryptoDetailsQuery } from "../services/cryptoApi"
+import {
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} from "../services/cryptoApi"
+import LineChart from "./LineChart"
 
 const { Title, Text } = Typography
 const { Option } = Select
@@ -24,10 +28,12 @@ const CryptoDetails = () => {
   const { coinId } = useParams()
   const [timePeriod, setTimePeriod] = useState("7d")
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId)
+  const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timePeriod })
   const cryptoDetails = data?.data?.coin
 
-  console.log(cryptoDetails.slug)
-  // console.log(data)
+  console.log(coinHistory)
+
+  if (isFetching) return "Loading"
 
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"]
 
@@ -108,7 +114,11 @@ const CryptoDetails = () => {
           <Option key={date}>{date}</Option>
         ))}
       </Select>
-      {/* line chart */}
+      <LineChart
+        coinHistory={coinHistory}
+        currentPrice={millify(cryptoDetails.price)}
+        coinName={cryptoDetails.name}
+      />
       <Col className="stats-container">
         <Col className="coin-value-statistics">
           <Col className="coin-value-statistics-heading">
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -14,12 +14,20 @@ export const cryptoApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: (count) => createRequest(`./coins?limit=${count}`),
+      query: (count) => createRequest(`/coins?limit=${count}`),
     }),
     getCryptoDetails: builder.query({
-      query: (coinId) => createRequest(`./coin/${coinId}`),
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timePeriod }) =>
+        createRequest(`/coin/${coinId}/history/${timePeriod}`),
     }),
   }),
 })
 
-export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi
